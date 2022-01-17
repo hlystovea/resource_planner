a421cfd30584fcf96b721ca951ec1fc9ebe07d3c
Evgenii Khlystov <hlystovea@gmail.com>
2022-01-18 06:53:14 +0700

    Added the Defect model

--- project/utils/html_to_pdf.py
+++ project/utils/html_to_pdf.py
@@ -6,7 +6,9 @@ from xhtml2pdf import pisa
 
 def render_to_pdf(html: str):
     result = BytesIO()
-    pdf = pisa.pisaDocument(BytesIO(html.encode('UTF-8')), result)
+    pdf = pisa.pisaDocument(
+        BytesIO(html.encode('UTF-8')), result, encoding='UTF-8'
+    )
     if pdf.err:
         return HttpResponse('Произошла ошибка <pre>' + html + '</pre>')
     return result
