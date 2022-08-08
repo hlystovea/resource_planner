023bf7ccf03d7713a737df9dd0ccb3515a32e28f
Evgenii Khlystov <hlystovea@gmail.com>
2022-08-08 16:14:54 +0700

    Added defects app

---
+++ project/defects/models.py
@@ -0,0 +1,142 @@
+from django.core.exceptions import ValidationError
+from django.db import models
+from django.utils.timezone import now
+from django.utils.translation import gettext_lazy as _
+from django_resized import ResizedImageField
+
+
+class Defect(models.Model):
+    date = models.DateField(
+        verbose_name=_('Дата обнаружения')
+    )
+    dispatch_object = models.ForeignKey(
+        to='hardware.Facility',
+        verbose_name=_('Объект диспетчеризации'),
+        on_delete=models.CASCADE,
+        related_name='defects'
+    )
+    connection = models.ForeignKey(
+        to='hardware.Connection',
+        verbose_name=_('Присоединение'),
+        on_delete=models.CASCADE,
+        related_name='defects'
+    )
+    hardware = models.ForeignKey(
+        to='hardware.Hardware',
+        verbose_name=_('Оборудование'),
+        on_delete=models.CASCADE,
+        related_name='defects'
+    )
+    employee = models.ForeignKey(
+        to='staff.Staff',
+        verbose_name=_('Сотрудник обнаруживший дефект'),
+        on_delete=models.PROTECT,
+        related_name='defects'
+    )
+    description = models.TextField(
+        verbose_name=_('Описание дефекта'),
+        max_length=1500,
+    )
+    image = ResizedImageField(
+        verbose_name=_('Фото'),
+        upload_to='defects/',
+        blank=True,
+        null=True,
+        size=[1280, 720],
+    )
+    effects = models.ManyToManyField(
+        to='defects.Effect',
+        verbose_name=_('Последствия дефекта'),
+        related_name='defects'
+    )
+    features = models.ManyToManyField(
+        to='defects.Feature',
+        verbose_name=_('Признаки дефекта'),
+        related_name='defects'
+    )
+    condition = models.ForeignKey(
+        to='defects.Condition',
+        verbose_name=_('Условие обнаружения'),
+        on_delete=models.PROTECT,
+        related_name='defects'
+    )
+    repair = models.TextField(
+        verbose_name=_('Выполненые мероприятия'),
+        max_length=500,
+        blank=True,
+        null=True,
+    )
+    repair_date = models.DateField(
+        verbose_name=_('Дата устранения'),
+        blank=True,
+        null=True,
+    )
+
+    class Meta:
+        ordering = ('-date', )
+        verbose_name = _('Дефект')
+        verbose_name_plural = _('Дефекты')
+
+    def clean(self):
+        errors = {}
+        if self.date > now():
+            errors['date'] = ValidationError(
+                _('Дата обнаружения дефекта не может быть в будущем')
+            )
+        if self.date > self.repair_date:
+            errors['repair_date'] = ValidationError(
+                _('Дата устранения дефекта не может быть раньше обнаружения')
+            )
+        if errors:
+            raise ValidationError(errors)
+
+
+class Effect(models.Model):
+    name = models.CharField(
+        verbose_name=_('Наименование'),
+        max_length=100,
+        unique=True
+    )
+    slug = models.SlugField(
+        verbose_name=_('Короткая ссылка'),
+        unique=True
+    )
+
+    class Meta:
+        ordering = ('name', )
+        verbose_name = _('Последствие дефекта')
+        verbose_name_plurar = _('Последствия дефекта')
+
+
+class Condition(models.Model):
+    name = models.CharField(
+        verbose_name=_('Наименование'),
+        max_length=100,
+        unique=True
+    )
+    slug = models.SlugField(
+        verbose_name=_('Короткая ссылка'),
+        unique=True
+    )
+
+    class Meta:
+        ordering = ('name', )
+        verbose_name = _('Условие обнаружения')
+        verbose_name_plurar = _('Условия обнаружения')
+
+
+class Feature(models.Model):
+    name = models.CharField(
+        verbose_name=_('Наименование'),
+        max_length=100,
+        unique=True
+    )
+    slug = models.SlugField(
+        verbose_name=_('Короткая ссылка'),
+        unique=True
+    )
+
+    class Meta:
+        ordering = ('name', )
+        verbose_name = _('Признак дефекта')
+        verbose_name_plurar = _('Признаки дефекта')
