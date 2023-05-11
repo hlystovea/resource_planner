35e8795cefdd058ef0ce7e1f878f09b4175d962d
Evgenii Khlystov <hlystovea@gmail.com>
2023-05-11 21:04:13 +0700

    Fixed tests

--- project/tests/fixtures/fixture_data.py
+++ project/tests/fixtures/fixture_data.py
@@ -1,25 +1,4 @@
 import pytest
-import tempfile
-
-
-@pytest.fixture
-def post(user):
-    from posts.models import Post
-    image = tempfile.NamedTemporaryFile(suffix=".jpg").name
-    return Post.objects.create(text='Тестовый пост 1', author=user, image=image)
-
-
-@pytest.fixture
-def group():
-    from posts.models import Group
-    return Group.objects.create(title='Тестовая группа 1', slug='test-link', description='Тестовое описание группы')
-
-
-@pytest.fixture
-def post_with_group(user, group):
-    from posts.models import Post
-    image = tempfile.NamedTemporaryFile(suffix=".jpg").name
-    return Post.objects.create(text='Тестовый пост 2', author=user, group=group, image=image)
 
 
 @pytest.fixture
@@ -99,8 +78,7 @@ def connection(facility):
 def group():
     from hardware.models import Group
     return Group.objects.create(
-        name='group',
-        abbreviation='g'
+        name='group'
     )
 
 
