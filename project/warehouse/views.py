02c8c3d8a57fdb183b9c1916be83c2641cf27046
Evgeniy Khlystov <hlystovea@gmail.com>
2023-08-28 10:57:04 +0700

    Fixed names of forms

--- project/warehouse/views.py
+++ project/warehouse/views.py
@@ -51,7 +51,8 @@ class StorageDetail(DetailView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['form'] = MaterialStorageForm()
+        context['storage_add_form'] = StorageAddForm()
+        context['material_add_form'] = MaterialStorageForm()
         return context
 
 
