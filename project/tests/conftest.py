214601cb30ce6320bc54fe6fcf6b81d0a0d09133
Evgeniy Khlystov <hlystovea@gmail.com>
2023-08-31 15:53:06 +0700

    Refactoring fixtures

--- project/tests/conftest.py
+++ project/tests/conftest.py
@@ -1,6 +1,6 @@
 pytest_plugins = [
     'tests.fixtures.fixture_user',
-    'tests.fixtures.fixture_data',
+    'tests.fixtures.fixture_defects',
     'tests.fixtures.fixture_staff',
     'tests.fixtures.fixture_warehouse',
 ]
