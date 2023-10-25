9fee8a498b8c9b084932ae702fbffdcc5b679916
Evgeniy Khlystov <hlystovea@gmail.com>
2023-10-25 09:31:50 +0700

    The bug with QueryDict class has been fixed

--- project/warehouse/templatetags/param_replace.py
+++ project/warehouse/templatetags/param_replace.py
@@ -10,6 +10,11 @@ def param_replace(context, **kwargs):
     https://stackoverflow.com/questions/22734695/next-and-before-links-for-a-django-paginated-query/22735278#22735278
     """
     query_params = context['request'].GET.copy()
-    query_params = query_params | kwargs
-    query_params = {key: value for key, value in query_params.items() if value}
+
+    for key, value in kwargs.items():
+        query_params[key] = value
+
+    for key in [key for key, value in query_params.items() if not value]:
+        del query_params[key]
+
     return query_params.urlencode()
