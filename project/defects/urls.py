4c3bb5f5932bd707c9c489f396d48c722e267922
Evgeniy Khlystov <hlystovea@gmail.com>
2023-11-16 16:19:57 +0700

    Added a slash at the end of the URL of the defect statistics

--- project/defects/urls.py
+++ project/defects/urls.py
@@ -13,5 +13,5 @@ urlpatterns = [
     path('<int:pk>/update', DefectUpdateView.as_view(), name='defect-update'),
     path('<int:pk>/delete', DefectDeleteView.as_view(), name='defect-delete'),
     path('create', DefectCreateView.as_view(), name='defect-create'),
-    path('statistics', DefectStatisticsView.as_view(), name='defect-statistics')  # noqa(E501)
+    path('statistics/', DefectStatisticsView.as_view(), name='defect-statistics')  # noqa(E501)
 ]
