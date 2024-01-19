16c6147278702c047fc4bfc42e2ec5affe45680f
Evgeniy Khlystov <hlystovea@gmail.com>
2024-01-19 11:42:56 +0700

    Refactoring the filters

--- project/warehouse/filters.py
+++ project/warehouse/filters.py
@@ -1,27 +1,14 @@
-from django_filters import FilterSet, NumberFilter
-
-from warehouse.models import Instrument, Material, Storage
+from django_filters import BooleanFilter, FilterSet, NumberFilter
 
 
 class InstrumentFilter(FilterSet):
     dept = NumberFilter(field_name='owner')
 
-    class Meta:
-        model = Instrument
-        fields = ['owner']
-
 
 class MaterialFilter(FilterSet):
     dept = NumberFilter(field_name='amount', lookup_expr='owner')
 
-    class Meta:
-        model = Material
-        fields = ['amount']
-
 
 class StorageFilter(FilterSet):
     storage = NumberFilter(field_name='parent_storage')
-
-    class Meta:
-        model = Storage
-        fields = ['parent_storage']
+    is_root = BooleanFilter(field_name='parent_storage', lookup_expr='isnull')
