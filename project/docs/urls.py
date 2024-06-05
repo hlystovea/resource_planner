88cc418e6cf2860c42155646375a5601d08d09cc
Evgeniy Khlystov <hlystovea@gmail.com>
2024-06-05 14:24:43 +0700

    Add: update and delete view for the ProtocolE2 model

--- project/docs/urls.py
+++ project/docs/urls.py
@@ -1,12 +1,16 @@
 from django.urls import path
 
-from docs.views import ProtocolE2CreateView, ProtocolE2DetailView, ProtocolE2ListView
+from docs.views import (ProtocolE2CreateView, ProtocolE2DeleteView,
+                        ProtocolE2DetailView, ProtocolE2ListView,
+                        ProtocolE2UpdateView)
 
 
 app_name = 'docs'
 
 urlpatterns = [
     path('protocol-e2/<int:pk>', ProtocolE2DetailView.as_view(), name='protocol_e2-detail'),  # noqa(E501)
-    path('protocol-e2/create', ProtocolE2CreateView.as_view(), name='protocol_e2-create'),
+    path('protocol-e2/create', ProtocolE2CreateView.as_view(), name='protocol_e2-create'),  # noqa(E501)
+    path('protocol-e2/<int:pk>/update', ProtocolE2UpdateView.as_view(), name='protocol_e2-update'),  # noqa(E501)
+    path('protocol-e2/<int:pk>/delete', ProtocolE2DeleteView.as_view(), name='protocol_e2-delete'),  # noqa(E501)
     path('protocol-e2/', ProtocolE2ListView.as_view(), name='protocol_e2-list'),  # noqa(E501)
 ]
--- project/docs/views.py
+++ project/docs/views.py
@@ -1,7 +1,8 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.db.models.functions import ExtractYear
 from django.urls import reverse_lazy
-from django.views.generic import CreateView, DetailView, ListView
+from django.views.generic import (CreateView, DeleteView,
+                                  DetailView, ListView, UpdateView)
 
 from core.utils import is_htmx
 from docs.filters import ProtocolE2Filter
@@ -15,7 +16,11 @@ class ProtocolE2ListView(ListView):
     def get_queryset(self):
         queryset = super().get_queryset()
         queryset = ProtocolE2Filter(self.request.GET, queryset=queryset).qs
-        queryset = queryset.select_related('connection')
+        queryset = queryset.select_related(
+            'connection', 'supervisor'
+        ).prefetch_related(
+            'signers'
+        )
         return queryset.order_by('-date')
 
     def get_template_names(self):
@@ -42,5 +47,18 @@ class ProtocolE2DetailView(DetailView):
 class ProtocolE2CreateView(LoginRequiredMixin, CreateView):
     model = ProtocolE2
     form_class = ProtocolE2Form
+    login_url = reverse_lazy('login')
+    success_url = reverse_lazy('docs:protocol_e2-list')
+
+
+class ProtocolE2UpdateView(LoginRequiredMixin, UpdateView):
+    model = ProtocolE2
+    form_class = ProtocolE2Form
+    login_url = reverse_lazy('login')
+    success_url = reverse_lazy('docs:protocol_e2-list')
+
+
+class ProtocolE2DeleteView(LoginRequiredMixin, DeleteView):
+    model = ProtocolE2
     login_url = reverse_lazy('login')
     success_url = reverse_lazy('docs:protocol_e2-list')
