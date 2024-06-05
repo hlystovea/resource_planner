486830bd8d8455dcdaa97d08f41df94e3db22833
Evgeniy Khlystov <hlystovea@gmail.com>
2024-06-05 13:42:18 +0700

    Add: setup an admin page for the ProtocolE2 model

--- project/docs/admin.py
+++ project/docs/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from django.db.models import CharField, TextField
+from django.forms import Textarea, TextInput
+from django.utils.translation import gettext_lazy as _
 
-# Register your models here.
+from .models import ProtocolE2
+
+
+class MixinAdmin(admin.ModelAdmin):
+    empty_value_display = _('-пусто-')
+    formfield_overrides = {
+        CharField: {'widget': TextInput(attrs={'size': 80})},
+        TextField: {'widget': Textarea(attrs={'rows': 8, 'cols': 80})},
+    }
+
+
+@admin.register(ProtocolE2)
+class ProtocolE2Admin(MixinAdmin):
+    list_display = ('id', 'date', 'connection', 'supervisor')
+    list_filter = ('date', 'connection')
+    date_hierarchy = 'date'
